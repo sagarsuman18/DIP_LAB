8f6368e47e21d9e2c2a6360a69fa2f1590b11003
sagarsuman18 <55747593+sagarsuman18@users.noreply.github.com>
2020-05-30 20:43:01 +0530

    Delete LPF_HPF.m

--- LPF_HPF.m
+++
@@ -1,8 +0,0 @@
-clc;clear;close all;
-I = imread('lenna.png');
-kernel = [-1 -1 -1;-1 8 -1;-1 -1 -1];%HPF
-kernel2 = 1/9 *([1,1,1;1,1,1;1,1,1]);%LPF
-LPF_Image = imfilter(I, kernel2, 'same');
-HPF_Image = imfilter(I, kernel, 'same');
-imshowpair(LPF_Image,HPF_Image,'montage');
-title('Low Pass Filter                                   High Pass Filter');
